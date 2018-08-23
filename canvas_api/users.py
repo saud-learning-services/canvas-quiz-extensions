e90bb5c2aceaab16390c9f66612664cf12a0a4b2
Tyler Cinkant <tyler.cinkant@ubc.ca>
2018-08-23 14:24:26 -0700

    added function to search user by course for instructors

--- canvas_api/users.py
+++ canvas_api/users.py
@@ -1,6 +1,7 @@
 import logging
 
 _GET_USER = "users/{user_id}"
+_GET_USER_BY_ENROLLMENT = "/courses/{course_id}/enrollments?sis_user_id[]={student_number}"
 
 def get_user_id(canvas, student_number):
     
@@ -14,7 +15,33 @@ def get_user_id(canvas, student_number):
     if 'errors' in response:
         if response['errors'][0]['message'] == "The specified resource does not exist.":
             return {"error":"user not found"}
+        elif response['errors'][0]['message'] == "user not authorized to perform that action":
+            return {"unauthorized":"user not authorized"}
         else:
             return {"error":response['errors'][0]['message']}
+
+    return response
+
+def get_user_id_by_enrollment(canvas, student_number, course_id):
+    
+    if not student_number:
+        return {"error":"null student_number"}
+    
+    if not course_id:
+        return {"error":"null course_id"}
+    
+    response = canvas.call_api(_GET_USER_BY_ENROLLMENT.format(course_id=course_id,
+                                                              student_number=student_number))
+    if not response:
+        return {"error":"null response"}
+    
+    if 'errors' in response:
+        if response['errors'][0]['message'] == "The specified resource does not exist.":
+            return {"error":"user not found"}
+        else:
+            return {"error":response['errors'][0]['message']}
+
+    if len(response) == 1:
+        return response[0]
 
     return response
--- extend_quiz.py
+++ extend_quiz.py
@@ -1,6 +1,6 @@
 from canvas_api import canvas
 from canvas_api.quizzes import extend_quiz_access
-from canvas_api.users import get_user_id
+from canvas_api.users import get_user_id, get_user_id_by_enrollment
 from canvas_api.util.file import read_from_csv, write_to_csv
 import logging
 import sys
@@ -23,7 +23,10 @@ if _INPUT_HAS_HEADERS == "False":
 
 _HEADERS = ['course_id', 'quiz_id', 'student_number', 'extra_time']
 
+_CHECK_BY_ENROLLMENTS = False
+
 def extend_user_access(input_list):
+    global _CHECK_BY_ENROLLMENTS
     output_content = [_HEADERS]
     output_content[0].append("status")
     for row in input_list:
@@ -33,8 +36,16 @@ def extend_user_access(input_list):
             student_number = row['student_number']
             extra_time = row['extra_time']
             output = [str(course_id), str(quiz_id), str(student_number), str(extra_time)]
-            
-            response = get_user_id(canvas, student_number)            
+
+            if _CHECK_BY_ENROLLMENTS:
+                response = get_user_id_by_enrollment(canvas, student_number, course_id)
+            else:
+                response = get_user_id(canvas, student_number)
+                
+            if 'unauthorized' in response:
+                _CHECK_BY_ENROLLMENTS = True
+                response = get_user_id_by_enrollment(canvas, student_number, course_id)
+                
             if 'error' in response:
                 output.append("FAILED: " + response['error'])
                 logging.warning(", ".join(output))
@@ -46,7 +57,11 @@ def extend_user_access(input_list):
                 output_content.append(output)
                 continue
             
-            user_id = response['id']
+            if _CHECK_BY_ENROLLMENTS:
+                user_id = response['user_id']
+            else:
+                user_id = response['id']
+                
             response = extend_quiz_access(canvas,course_id, quiz_id, user_id, extra_time)            
             if 'error' in response:
                 output.append("FAILED: " + response['error'])
